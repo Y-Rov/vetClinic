74c83d011d734a531e3c8029ee93772d7f9fe591
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-15 19:03:54 +0300

    Add UserID and Wage properties in Salary class

--- Core/Entities/Salary.cs
+++ Core/Entities/Salary.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Core.Entities
 {
+    //Class Salary contains info about employees salary
     public class Salary
     {
+        public int UserId { get; set; }
+        
+        //public User User_obj { get; set; }  //do we need this?
+
+        public decimal Wage { get; set; }
+
     }
 }
