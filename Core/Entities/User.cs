0a49be17cb80fbbf5f28a4fc51a611c7721f8c4b
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-15 18:56:48 +0300

    First/LastName and EmailAddress are nullable types now

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -9,9 +9,9 @@ namespace Core.Entities
     public class User
     {
         public int ID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string EmailAddress { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? EmailAddress { get; set; }
         public DateTime BirthDate { get; set;}
 
     }
