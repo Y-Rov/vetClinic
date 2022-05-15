6080f2c62e2e159836648dc19c095a572598696e
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-15 18:55:03 +0300

    Creat Entities folder. Add user plug_class_file and salary_class_file

---
+++ Core/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime BirthDate { get; set;}
+
+    }
+}
