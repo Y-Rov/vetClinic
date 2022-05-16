4a95afcfa41ce1d222cc98aa63889043163a241d
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-16 18:24:20 +0300

    Add FinancialService  in Application\Services Add IFinancialService  in Core\Interfaces

---
+++ Core/Interfaces/IFinancialService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IFinancialService
+    {
+        public void GenerateFinStatementForMonth();
+        public void GenerateFinStatementForHalfOfYear();
+        public void GenerateFinStatementForYear();
+        public void SetSalary(int userId, decimal value);
+        public void ChangeSalary(int userId, decimal value);
+        public decimal GetSalary(int userId);
+        public IEnumerable<Salary> GetAllSalary();
+
+    }
+}
