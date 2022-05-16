eff1bcd8c1347373428503018a4c4c883d615235
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-05-16 14:07:43 +0300

    Add Animal class

---
+++ Core/Entities/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Animal
+    {
+        public int Id { get; set; }
+        public string? NickName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int UserId { get; set; }
+    }
+}
