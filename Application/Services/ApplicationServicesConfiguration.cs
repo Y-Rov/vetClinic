996803bddcb495a0083f71d24b3d8772c9d44a66
froizerner <froizerner@gmail.com>
2022-05-17 17:54:02 +0300

    Add ApplicationServicesConfiguration

---
+++ Application/Services/ApplicationServicesConfiguration.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ApplicationServicesConfiguration
+{
+    public static void AddApplicationService(this IServiceCollection services)
+    {
+
+    }
+}
