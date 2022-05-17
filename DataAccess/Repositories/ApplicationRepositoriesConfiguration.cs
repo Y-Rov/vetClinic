be19f07eeead016edfd9596f28b4ae94061440cf
froizerner <froizerner@gmail.com>
2022-05-17 17:59:18 +0300

    add ApplicationRepositoriesConfiguration

---
+++ DataAccess/Repositories/ApplicationRepositoriesConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess
+{
+    public static class ApplicationRepositoriesConfiguration
+    {
+        public static void AddApplicationRepositories(this IServiceCollection repositories)
+        {
+        }
+    }
+}
