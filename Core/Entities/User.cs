71da6d41f417efefb5fe3a8338e3dda1e6a90bd7
Vitalik Knysh <knish.vital@gmail.com>
2022-05-18 09:54:11 +0300

    Modify User entity

--- Core/Entities/User.cs
+++ Core/Entities/User.cs
@@ -8,11 +8,16 @@ namespace Core.Entities
 {
     public class User
     {
-        public int ID { get; set; }
+        public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
-        public string? EmailAddress { get; set; }
+        public string? Email { get; set; }
         public DateTime BirthDate { get; set;}
 
+        // Salary
+        // Address
+
+        // public IList<Animal> Animals { get; set; }
+        // public IList<Appointment> Appointments { get; set; }
     }
 }
