b9cb4fea7bea245a25893bd8f0d3ee33f783302a
= <--add>
2022-05-18 14:51:00 +0300

    addDbContextClass

---
+++ DataAccess/Context/ClinicContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class ClinicContext : DbContext
+    {
+        public ClinicContext() { }
+        public ClinicContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ᓚᘏᗢ For fluent API configurations
+        }
+    }
+}
