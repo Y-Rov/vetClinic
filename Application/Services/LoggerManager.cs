a266c5b835f1f1f22ef747ce58ec2579870bfbdb
froizerner <froizerner@gmail.com>
2022-05-18 09:05:10 +0300

    Added realization to ILoggerManager and LoggerManager

--- Application/Services/LoggerManager.cs
+++ Application/Services/LoggerManager.cs
@@ -1,13 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Interfaces;
+using NLog;
 
 namespace Application.Services
 {
-    public class LoggerManager
+    public class LoggerManager : ILoggerManager
+
     {
+        private static NLog.ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message) => logger.Debug(message);
+
+
+        public void LogError(string message) => logger.Error(message);
+
+
+        public void LogInfo(string message) => logger.Info(message);
+
+
+        public void LogWarn(string message) => logger.Warn(message);
 
     }
 }
--- Core/Interfaces/ILoggerManager.cs
+++ Core/Interfaces/ILoggerManager.cs
@@ -8,6 +8,9 @@ namespace Core.Interfaces
 {
     public interface ILoggerManager
     {
-
+        void LogInfo(string message);
+        void LogWarn(string message);
+        void LogDebug(string message);
+        void LogError(string message);
     }
 }
