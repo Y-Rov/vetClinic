1001d813b8fd812e95e5208b9b2c76af751b86bd
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-18 20:21:28 +0300

    Add AutoMapperProfile

---
+++ WebApi/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core.DTO;
+using Core.Entities;
+
+namespace WebApi.AutoMapper
+{
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<UserDTO, User>().ReverseMap(); //reverse so the both direction
+        }
+    }
+}
