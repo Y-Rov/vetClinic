d60becff81c2e0c70523def078689403bcf85481
Maxim Kravchuk <54956796+MaxKravchuk@users.noreply.github.com>
2022-05-18 18:13:54 +0300

    Set up class, service and Iservice for the Animal

---
+++ Application/Services/AniamlService.cs
@@ -0,0 +1,43 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AniamlService : IAnimalService
+    {
+        public void AddNewPet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeletePet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Animal> GetAllAnimals()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Animal GetAnimalById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdatePet()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Appointment> GetAllAppointmentsWithAnimalId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
