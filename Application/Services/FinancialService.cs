bc343753aa2db6a0a35d39c88088f8510d3905f0
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-18 12:15:19 +0300

    Add FinancialService_class and IFinancialService_interface

---
+++ Application/Services/FinancialService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class FinancialService
+    {
+        public void GenerateFinancialStatementForMonth()
+        {
+            throw new NotImplementedException();
+        }
+        public void GenerateFinancialStatementForHalfOfYear()
+        {
+            throw new NotImplementedException();
+        }
+        public void GenerateFinancialStatementForYear()
+        {
+            throw new NotImplementedException();
+        }
+        public decimal GetSalaryById(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public decimal SetSalaryById(int id, decimal value)
+        {
+            throw new NotImplementedException();
+        }
+        //public IEnumerable<Salary> GetSalaries()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ Core/Entities/Salary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Salary
+    {
+        public int UserId { get; set; }
+        public User EmployeeUser { get; set; }
+        public decimal Value { get; set; }
+    }
+}
