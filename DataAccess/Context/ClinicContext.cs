5d2b88aa53d9ed6de23f2af6e405a4547553e5c4
= <--add>
2022-05-18 14:55:49 +0300

    addDbSets

--- DataAccess/Context/ClinicContext.cs
+++ DataAccess/Context/ClinicContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,15 @@ namespace DataAccess.Context
 {
     public class ClinicContext : DbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Animal> Animals { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Salary> Salaries { get; set; }
+        public DbSet<Specialization> Specializations { get; set; }
+        public DbSet<User> Users { get; set; }
         public ClinicContext() { }
         public ClinicContext(DbContextOptions options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
