f3cb7f3dfa99bde847ffe40c569a25e900d30db1
froizerner <froizerner@gmail.com>
2022-05-18 08:55:37 +0300

    Setup Logger(Nlog) with DI

--- Application/Services/ApplicationServicesConfiguration.cs
+++ Application/Services/ApplicationServicesConfiguration.cs
@@ -1,11 +1,12 @@
-﻿
-
+﻿using Application.Services;
+using Core.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using NLog;
 
 public static class ApplicationServicesConfiguration
 {
     public static void AddApplicationService(this IServiceCollection services)
     {
-
+        services.AddSingleton<ILoggerManager,LoggerManager>();
     }
 }
--- Host/Program.cs
+++ Host/Program.cs
@@ -1,6 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
 
+var builder = WebApplication.CreateBuilder(args);
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 // Add services to the container.
+builder.Services.AddApplicationService();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
