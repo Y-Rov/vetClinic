3ec476ad012fae8139a02703243da280a80fed92
= <--add>
2022-05-18 17:24:51 +0300

    changes

--- DataAccess/Context/ClinicContext.cs
+++ DataAccess/Context/ClinicContext.cs
@@ -19,8 +19,7 @@ namespace DataAccess.Context
         public DbSet<Salary> Salaries { get; set; }
         public DbSet<Specialization> Specializations { get; set; }
         public DbSet<User> Users { get; set; }
-        public ClinicContext() { }
-        public ClinicContext(DbContextOptions options) : base(options) { }
+        public ClinicContext(DbContextOptions<ClinicContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // ᓚᘏᗢ For fluent API configurations
