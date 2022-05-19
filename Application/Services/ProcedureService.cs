6823ee622d1d9b478311d48eb7ccac57ded3bda2
sefirus <sashasam348@gmail.com>
2022-05-19 16:56:36 +0300

    Filled procedure entity, added procedureService class and interface

---
+++ Application/Services/ProcedureService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+
+namespace Application.Services;
+
+public class ProcedureService : IProcedureService
+{
+    public void CreateNewProcedure(Procedure procedure)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateProcedure(int oldProcedureId, Procedure newProcedure)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteProcedure(int procedureId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Procedure GetById(int procedureId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Procedure> GetAllProcedures()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Core/Interfaces/Services/IProcedureService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Services;
+
+public interface IProcedureService
+{
+    public void CreateNewProcedure(Procedure procedure);
+    public void UpdateProcedure(int oldProcedureId, Procedure newProcedure);
+    public void DeleteProcedure(int procedureId);
+
+    public Procedure GetById(int procedureId);
+    
+    public IEnumerable<Procedure> GetAllProcedures();
+}
