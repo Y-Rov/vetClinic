21185499d7fd2f0469be29ac160559d26d563d74
= <--add>
2022-05-20 18:51:49 +0300

    resolved application services

---
+++ Application/Configuration/ApplicationServicesConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services;
+using Core.Interfaces.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Configuration
+{
+    public static class ApplicationServicesConfiguration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAnimalService, AnimalService>();
+        }
+    }
+}
