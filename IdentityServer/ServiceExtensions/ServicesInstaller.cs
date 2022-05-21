aa5ef6086ff1134363af1a1dcd110163119e63a4
froizerner <froizerner@gmail.com>
2022-05-21 19:12:30 +0300

    Resolved conflict with ServiceExtensions

--- IdentityServer/ServiceExtensions/ServicesInstaller.cs
+++ IdentityServer/ServiceExtensions/ServicesInstaller.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Linq;
 using Core.Entities;
 using DataAccess;
+using DataAccess.Context;
 using IdentityServer.Services;
 using IdentityServer4.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using DataAccess.Context;
 
 namespace IdentityServer.ServiceExtensions;
 
@@ -14,31 +14,27 @@ public static class ServicesInstaller
 {
     public static void AddServices(this IServiceCollection services, IConfiguration config)
     {
-        // services.AddCors(opts =>
-        //      opts.AddPolicy("AllowAll",
-        //          p => p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
-        
-        services.AddDbContext<ClinicContext>(opts => 
+        services.AddDbContext<ClinicContext>(opts =>
             opts.UseSqlServer(config.GetConnectionString("Default")));
-        
+
         services.AddIdentity<User, IdentityRole<int>>(opts =>
-            {
-                opts.Password.RequireDigit = false;
-                opts.Password.RequireLowercase = false;
-                opts.Password.RequireNonAlphanumeric = false;
-                opts.Password.RequireUppercase = false;
-                opts.Password.RequiredLength = 5;
-            })
+        {
+            opts.Password.RequireDigit = false;
+            opts.Password.RequireLowercase = false;
+            opts.Password.RequireNonAlphanumeric = false;
+            opts.Password.RequireUppercase = false;
+            opts.Password.RequiredLength = 5;
+        })
             .AddEntityFrameworkStores<ClinicContext>()
             .AddDefaultTokenProviders();
 
         var assembly = typeof(Program).Assembly.GetName().Name;
-        
+
         services.AddIdentityServer()
             .AddDeveloperSigningCredential()
             .AddOperationalStore(opts =>
             {
-                opts.ConfigureDbContext = builder => builder.UseSqlServer(config.GetConnectionString("Default"), 
+                opts.ConfigureDbContext = builder => builder.UseSqlServer(config.GetConnectionString("Default"),
                     opt => opt.MigrationsAssembly(assembly));
             })
             .AddInMemoryIdentityResources(Config.GetIdentityResources())
