7594c4d927d43e28f15578376a7000476cec4340
= <--add>
2022-05-21 18:47:01 +0300

    realization of specialization repo has added

--- DataAccess/Context/ClinicContext.cs
+++ DataAccess/Context/ClinicContext.cs
@@ -5,10 +5,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Context
 {
-    public class ClinicContext : IdentityDbContext<User, IdentityRole<int>,int>
+    public class ClinicContext : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public DbSet<Animal> Animals { get; set; }
-        
+        public DbSet<Specialization> Specializations { get; set; }
 
         public ClinicContext(DbContextOptions<ClinicContext> options) 
             : base(options) { }
