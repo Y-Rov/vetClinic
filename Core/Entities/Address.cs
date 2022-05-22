77d3ff69497444631f805291e4ba18779d38c93d
YuraRov <yura.rov555@gmail.com>
2022-05-22 18:15:39 +0300

    Renamed FlatNumber property to ApartmentNumber

--- Core/Entities/Address.cs
+++ Core/Entities/Address.cs
@@ -7,7 +7,7 @@
         public string? Street { get; set; }
         public string? House { get; set; }
         public byte? BuildingNumber { get; set; }
-        public ushort? FlatNumber { get; set; }
+        public ushort? ApartmentNumber { get; set; }
         public int ZipCode { get; set; }
         public int UserId { get; set; }
         public User? User { get; set; }
