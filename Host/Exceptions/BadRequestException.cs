5e009c462f13716eb14707960152d100b62a5597
froizerner <froizerner@gmail.com>
2022-05-22 21:37:28 +0300

    Added custom exceptions

---
+++ Host/Exceptions/BadRequestException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public new string Message { get; set; }
+        public BadRequestException()
+        {
+            Message = "Illegal request";
+        }
+        public BadRequestException(string message) : base(message)
+        {
+            Message = message;
+        }
+        public BadRequestException(string message, Exception inner) : base(message, inner)
+        {
+            Message = message;
+        }
+    }
+}
---
+++ Host/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host.Exceptions
+{
+    public class UnauthorizedException:Exception
+    {
+        public new string Message { get; set; }
+        public UnauthorizedException()
+        {
+            Message = "Access is denied due to invalid credentials";
+        }
+        public UnauthorizedException(string message) : base(message)
+        {
+            Message = message;
+        }
+        public UnauthorizedException(string message, Exception inner) : base(message, inner)
+        {
+            Message = message;
+        }
+    }
+}
