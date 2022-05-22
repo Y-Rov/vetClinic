f5221cc1e5595b90feef2d8831abf52de7ac7fa2
froizerner <froizerner@gmail.com>
2022-05-22 22:12:07 +0300

    Deleted extra usings

--- WebApi/Exceptions/BadRequestException.cs
+++ WebApi/Exceptions/BadRequestException.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebApi.Exceptions
+﻿namespace WebApi.Exceptions
 {
     public class BadRequestException : Exception
     {
