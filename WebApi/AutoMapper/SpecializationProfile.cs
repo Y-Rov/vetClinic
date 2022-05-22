3af524ba476965a67e188904cbd1edf9027ada31
= <--add>
2022-05-23 00:56:43 +0300

    added specializationDTO

---
+++ WebApi/AutoMapper/SpecializationProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Core.DTO;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.AutoMapper
+{
+    public class SpecializationProfile : Profile
+    {
+        public SpecializationProfile()
+        {
+            CreateMap<Specialization, SpecializationDTO>()
+                .ForMember(specialization => specialization.Id, input =>
+                    input.MapFrom(specDTO => specDTO.Id))
+                .ReverseMap();
+        }
+    }
+}
