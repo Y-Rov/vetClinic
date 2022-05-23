f4edf714c56792cc7849d35e4cf7d6be5f4a6fd8
YuraRov <yura.rov555@gmail.com>
2022-05-23 14:19:29 +0300

    Added Address service carcass

--- Core/Interfaces/Services/IAddressService.cs
+++ Core/Interfaces/Services/IAddressService.cs
@@ -1,6 +1,13 @@
-﻿namespace Core.Interfaces.Services
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Services
 {
     public interface IAddressService
     {
+        Task<Address> GetAddressByIdAsync(int id);
+        Task<IEnumerable<Address>> GetAllAddressesAsync();
+        Task CreateAddressAsync(Address portfolio);
+        Task<Address> UpdateAddressAsync(Address portfolio);
+        Task DeleteAddressAsync(int id);
     }
 }
