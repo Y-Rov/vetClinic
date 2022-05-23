6e81f143b1d87c82a6a7be06fd0765f5f62fd907
= <--add>
2022-05-23 11:16:17 +0300

    removed usings

--- WebApi/AutoMapper/SpecializationProfile.cs
+++ WebApi/AutoMapper/SpecializationProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using Core.DTO;
 using Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WebApi.AutoMapper
 {
