545fcac7f6b916c8b3f0b3804d215df2cea25680
froizerner <froizerner@gmail.com>
2022-05-23 20:19:53 +0300

    Added ctor to custom exception for ValidationFailure

--- WebApi/Exceptions/BadRequestException.cs
+++ WebApi/Exceptions/BadRequestException.cs
@@ -1,19 +1,31 @@
-﻿namespace WebApi.Exceptions
+﻿using FluentValidation.Results;
+
+namespace WebApi.Exceptions
 {
     public class BadRequestException : Exception
     {
+        public List<ValidationFailure>? ValidationFailure;
         public new string Message { get; set; }
+
         public BadRequestException()
         {
             Message = "Illegal request";
         }
+
         public BadRequestException(string message) : base(message)
         {
             Message = message;
         }
+
         public BadRequestException(string message, Exception inner) : base(message, inner)
         {
             Message = message;
+        }
+
+        public BadRequestException(List<ValidationFailure> validationFailures)
+        {
+            Message = "Illegal request";
+            ValidationFailure = validationFailures;
         }
     }
 }
