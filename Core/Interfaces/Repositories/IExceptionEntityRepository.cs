3881a22a1119d2e805bde0f15e1deecb9e86704c
froizerner <froizerner@gmail.com>
2022-05-23 12:04:45 +0300

    Added repository exception logic

---
+++ Core/Interfaces/Repositories/IExceptionEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Repositories
+{
+    public interface IExceptionEntityRepository
+    {
+        Task<IEnumerable<ExceptionEntity>> GetAllAsync();
+        Task<IEnumerable<object>> GroupAllByName();
+        Task<ExceptionEntity> GetById(int id);
+        Task<IEnumerable<ExceptionEntity>> GetForThisDay();
+
+    }
+}
---
+++ DataAccess/Repositories/ExceptionEntityRepository.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace DataAccess.Repositories
+{
+    public class ExceptionEntityRepository : IExceptionEntityRepository
+    {
+        private readonly ClinicContext _clinicContext;
+        public ExceptionEntityRepository(ClinicContext clinicContext)
+        {
+            _clinicContext = clinicContext;
+        }
+
+        public async Task<IEnumerable<ExceptionEntity>> GetAllAsync()
+        {
+            return await _clinicContext.Exceptions.ToListAsync();
+        }
+
+        public async Task<ExceptionEntity> GetById(int id)
+        {
+            return await _clinicContext.Exceptions.FirstOrDefaultAsync(ex => ex.Id == id);
+        }
+
+        public async Task<IEnumerable<ExceptionEntity>> GetForThisDay()
+        {
+            return await _clinicContext.Exceptions.Where(ex => ex.DateTime.Day == DateTime.Today.Day).ToListAsync();
+        }
+
+        public async Task<IEnumerable<object>> GroupAllByName()
+        {
+            return await _clinicContext.Exceptions.GroupBy(ex => ex.Name).Select(g => new { Name = g.Key, Count = g.Count() }).ToListAsync();
+        }
+    }
+}
