ae588d1dba272753aa9cc11f122755250cde4814
froizerner <froizerner@gmail.com>
2022-05-23 12:04:06 +0300

    Added exception service logic

---
+++ Application/Services/ExceptionEntityService.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+
+namespace Application.Services
+{
+    public class ExceptionEntityService : IExceptionEntityService
+    {
+        private readonly IExceptionEntityRepository _exceptionEntityRepository;
+        public ExceptionEntityService(IExceptionEntityRepository exceptionEntityRepository)
+        {
+            _exceptionEntityRepository = exceptionEntityRepository;
+        }
+
+        public async Task<IEnumerable<ExceptionEntity>> GetAllAsync()
+        {
+            return await _exceptionEntityRepository.GetAllAsync();
+        }
+
+        public async Task<ExceptionEntity> GetById(int id)
+        {
+            return await _exceptionEntityRepository.GetById(id);
+        }
+
+        public async Task<IEnumerable<ExceptionEntity>> GetForThisDay()
+        {
+            return await _exceptionEntityRepository.GetForThisDay();
+        }
+
+        public async Task<IEnumerable<object>> GroupAllByName()
+        {
+            return await _exceptionEntityRepository.GroupAllByName();
+        }
+    }
+}
---
+++ Core/Interfaces/Services/IExceptionEntityService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+
+namespace Core.Interfaces.Services
+{
+    public interface IExceptionEntityService
+    {
+        Task<IEnumerable<ExceptionEntity>> GetAllAsync();
+        Task<IEnumerable<object>> GroupAllByName();
+        Task<ExceptionEntity> GetById(int id);
+        Task<IEnumerable<ExceptionEntity>> GetForThisDay();
+    }
+}
