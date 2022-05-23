8250b3f7ba6ad6131f6d5590e48739c58aec4d5c
froizerner <froizerner@gmail.com>
2022-05-23 13:04:17 +0300

    Added logging

--- WebApi/Controllers/ExceptionController.cs
+++ WebApi/Controllers/ExceptionController.cs
@@ -11,31 +11,42 @@ namespace WebApi.Controllers
     public class ExceptionController : ControllerBase
     {
         private readonly IExceptionEntityService _exceptionEntityService;
-
-        public ExceptionController(IExceptionEntityService exceptionEntityService)
+        private readonly ILoggerManager _loggerManager;
+        public ExceptionController(IExceptionEntityService exceptionEntityService, ILoggerManager loggerManager)
         {
             _exceptionEntityService = exceptionEntityService;
+            _loggerManager = loggerManager;
         }
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<ExceptionEntity>>> GetAllAsync()
         {
+            _loggerManager.LogInfo("Open ExceptionController action GetAllAsync()");
             var allExceptions = await _exceptionEntityService.GetAllAsync();
 
             if (allExceptions == null)
+            {
+                _loggerManager.LogError("Error (NotFoundException) at ExceptionController action GetAllAsync()");
                 throw new NotFoundException();
+            }
 
+            _loggerManager.LogInfo("Exit ExceptionController action GetAllAsync()");
             return Ok(allExceptions);
         }
 
         [HttpGet("Stats")]
-        public async Task<ActionResult<IEnumerable<object>>> GetStats()
+        public async Task<ActionResult<IEnumerable<object>>> GetStatsAsync()
         {
+            _loggerManager.LogInfo("Open ExceptionController action GetStatsAsync()");
             var exceptionsStats = await _exceptionEntityService.GetStatsAsync();
 
             if (exceptionsStats == null)
+            {
+                _loggerManager.LogError("Error (NotFoundException) at ExceptionController action GetStatsAsync()");
                 throw new NotFoundException();
+            }
 
+            _loggerManager.LogInfo("Exit ExceptionController action GetStatsAsync()");
             return Ok(exceptionsStats);
 
         }
@@ -43,37 +54,50 @@ namespace WebApi.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<ExceptionEntity>> GetByIdAsync(int id)
         {
+            _loggerManager.LogInfo("Open ExceptionController action GetByIdAsync()");
             var exception = await _exceptionEntityService.GetByIdAsync(id);
 
             if (exception == null)
+            {
+                _loggerManager.LogError("Error (NotFoundException) at ExceptionController action GetByIdAsync()");
                 throw new NotFoundException();
+            }
 
+            _loggerManager.LogInfo("Exit ExceptionController action GetByIdAsync()");
             return Ok(exception);
 
         }
 
         [HttpGet("Today")]
-        public async Task<ActionResult<IEnumerable<ExceptionEntity>>> GetToday()
+        public async Task<ActionResult<IEnumerable<ExceptionEntity>>> GetTodayAsync()
         {
+            _loggerManager.LogInfo("Open ExceptionController action GetTodayAsync()");
             var exceptions = await _exceptionEntityService.GetTodayAsync();
 
             if (exceptions == null)
+            {
+                _loggerManager.LogError("Error (NotFoundException) at ExceptionController action GetTodayAsync()");
                 throw new NotFoundException();
+            }
 
+            _loggerManager.LogInfo("Exit ExceptionController action GetTodayAsync()");
             return Ok(exceptions);
 
         }
 
         [HttpGet("Stats/Today")]
-        public async Task<ActionResult<IEnumerable<object>>> GetTodayStats()
+        public async Task<ActionResult<IEnumerable<object>>> GetTodayStatsAsync()
         {
+            _loggerManager.LogInfo("Open ExceptionController action GetTodayStatsAsync()");
             var exceptionsStats = await _exceptionEntityService.GetTodayStatsAsync();
 
-            if (exceptionsStats  == null)
+            if (exceptionsStats == null)
+            {
+                _loggerManager.LogError("Error (NotFoundException) at ExceptionController action GetTodayStatsAsync()");
                 throw new NotFoundException();
+            }
 
-   
-
+            _loggerManager.LogInfo("Exit ExceptionController action GetTodayStatsAsync()");
             return Ok(exceptionsStats);
 
         }
