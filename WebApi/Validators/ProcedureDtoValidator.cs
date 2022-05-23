19940194c1fc2e31b4682bec15f875f0e1a6b187
sefirus <sashasam348@gmail.com>
2022-05-23 17:18:08 +0300

    Finished DB configuring, created and implemented (I)procedureService, created and implemented ProcedureController with all procedureDto and viewModel and all necessary mapping and validation

---
+++ WebApi/Validators/ProcedureDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using Core.DTO.ProcedureDTOs;
+using FluentValidation;
+
+namespace WebApi.Validators;
+
+public class ProcedureDtoValidator : AbstractValidator<ProcedureDTO>
+{
+    public ProcedureDtoValidator()
+    {
+        RuleFor(dto => dto.Cost)
+            .GreaterThan(0);
+        RuleFor(dto => dto.Description)
+            .Length(10, 250);
+        RuleFor(dto => dto.Name)
+            .Length(10, 100);
+        RuleFor(dto => dto.DurationInMinutes)
+            .GreaterThan(0);
+    }
+}
