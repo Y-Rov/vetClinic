076a524a7e7d1953827afac07c4279900c13d730
YuraRov <yura.rov555@gmail.com>
2022-05-23 13:23:38 +0300

    Added Portfolio and Address repository carcass

--- DataAccess/Repositories/AddressRepository.cs
+++ DataAccess/Repositories/AddressRepository.cs
@@ -1,8 +1,42 @@
-﻿using Core.Interfaces.Repositories;
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Repositories
 {
     public class AddressRepository : IAddressRepository
     {
+        private readonly ClinicContext _clinicContext;
+
+        public AddressRepository(ClinicContext clinicContext) => _clinicContext = clinicContext;
+
+        public async Task<Address> GetAddressByIdAsync(int id)
+        {
+            return await _clinicContext.Addresses.Where(address => address.Id == id).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Address>> GetAllAddressesAsync()
+        {
+            return await _clinicContext.Addresses.ToListAsync();
+        }
+
+        public async Task CreateAddressAsync(Address address)
+        {
+            await _clinicContext.Addresses.AddAsync(address);
+        }
+
+        public async Task<Address> UpdateAddressAsync(Address address)
+        {
+            _clinicContext.Addresses.Update(address);
+            await _clinicContext.SaveChangesAsync();
+            return address;
+        }
+
+        public async Task DeleteAddressAsync(int id)
+        {
+            _clinicContext.Remove(GetAddressByIdAsync(id));
+            await _clinicContext.SaveChangesAsync();
+        }
     }
 }
