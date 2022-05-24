9ae7b35b1109872c6b36b1255bf98b25cec6cf07
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-24 15:17:27 +0300

    Added migraitons folder

---
+++ DataAccess/Migrations/20220522174222_SetRestrictOptToOnDeleteForAnimal.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class SetRestrictOptToOnDeleteForAnimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animals_AspNetUsers_OwnerId",
+                table: "Animals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_Animals_AnimalId",
+                table: "Appointment");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_AspNetUsers_UserId",
+                table: "Appointment");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Appointment",
+                table: "Appointment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Appointment_UserId",
+                table: "Appointment");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Appointment");
+
+            migrationBuilder.RenameTable(
+                name: "Appointment",
+                newName: "Appointments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Appointment_AnimalId",
+                table: "Appointments",
+                newName: "IX_Appointments_AnimalId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Appointments",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Disease",
+                table: "Appointments",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "MeetHasOccureding",
+                table: "Appointments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Appointments",
+                table: "Appointments",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(85)", maxLength: 85, nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(85)", maxLength: 85, nullable: false),
+                    House = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    BuildingNumber = table.Column<byte>(type: "tinyint", nullable: true),
+                    ApartmentNumber = table.Column<short>(type: "smallint", nullable: true),
+                    ZipCode = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Addresses_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentUsers",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentUsers", x => new { x.UserId, x.AppointmentId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentUsers_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentUsers_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(512)", maxLength: 512, nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Portfolios_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Procedures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Cost = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DurationInMinutes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedures", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Specializations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specializations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppointmentProcedures",
+                columns: table => new
+                {
+                    AppointmentId = table.Column<int>(type: "int", nullable: false),
+                    ProcedureId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppointmentProcedures", x => new { x.ProcedureId, x.AppointmentId });
+                    table.ForeignKey(
+                        name: "FK_AppointmentProcedures_Appointments_AppointmentId",
+                        column: x => x.AppointmentId,
+                        principalTable: "Appointments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AppointmentProcedures_Procedures_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "Procedures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcedureSpecializations",
+                columns: table => new
+                {
+                    ProcedureId = table.Column<int>(type: "int", nullable: false),
+                    SpecializationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcedureSpecializations", x => new { x.ProcedureId, x.SpecializationId });
+                    table.ForeignKey(
+                        name: "FK_ProcedureSpecializations_Procedures_ProcedureId",
+                        column: x => x.ProcedureId,
+                        principalTable: "Procedures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProcedureSpecializations_Specializations_SpecializationId",
+                        column: x => x.SpecializationId,
+                        principalTable: "Specializations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserSpecializations",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SpecializationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserSpecializations", x => new { x.UserId, x.SpecializationId });
+                    table.ForeignKey(
+                        name: "FK_UserSpecializations_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserSpecializations_Specializations_SpecializationId",
+                        column: x => x.SpecializationId,
+                        principalTable: "Specializations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "86d13b47-b0b3-4e91-9e74-1ece2346c88b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "7e106b2e-6f9d-4e74-a2be-90b1c595f6ce");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "a78df7a0-3809-43fc-b618-7f2860bc7720");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "117af209-d5ed-42a3-83d7-19df8bdd9e66");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "e00d4dd3-d8b5-4d93-94f8-986fd266d5b5", "AQAAAAEAACcQAAAAEP4FMtudPDpHEtK0uN/iuHWkjPpXxMsXqe8+lZ2YHYiZ3GvivJWiFRNaWhIxYmXfjA==", "c9a80641-f4a1-4ca0-b3a3-07d1a907e8fe" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Addresses_UserId",
+                table: "Addresses",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentProcedures_AppointmentId",
+                table: "AppointmentProcedures",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppointmentUsers_AppointmentId",
+                table: "AppointmentUsers",
+                column: "AppointmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolios_UserId",
+                table: "Portfolios",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcedureSpecializations_SpecializationId",
+                table: "ProcedureSpecializations",
+                column: "SpecializationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSpecializations_SpecializationId",
+                table: "UserSpecializations",
+                column: "SpecializationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animals_AspNetUsers_OwnerId",
+                table: "Animals",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointments_Animals_AnimalId",
+                table: "Appointments",
+                column: "AnimalId",
+                principalTable: "Animals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Animals_AspNetUsers_OwnerId",
+                table: "Animals");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointments_Animals_AnimalId",
+                table: "Appointments");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "AppointmentProcedures");
+
+            migrationBuilder.DropTable(
+                name: "AppointmentUsers");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+
+            migrationBuilder.DropTable(
+                name: "ProcedureSpecializations");
+
+            migrationBuilder.DropTable(
+                name: "UserSpecializations");
+
+            migrationBuilder.DropTable(
+                name: "Procedures");
+
+            migrationBuilder.DropTable(
+                name: "Specializations");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Appointments",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "Disease",
+                table: "Appointments");
+
+            migrationBuilder.DropColumn(
+                name: "MeetHasOccureding",
+                table: "Appointments");
+
+            migrationBuilder.RenameTable(
+                name: "Appointments",
+                newName: "Appointment");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Appointments_AnimalId",
+                table: "Appointment",
+                newName: "IX_Appointment_AnimalId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Appointment",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Appointment",
+                table: "Appointment",
+                column: "Id");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "84b92bc4-943c-4d68-8a38-87bfa816a9e3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "d5ee4b82-2db3-44f0-8f28-32300e1b4ad2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "a2ae4d02-aa2a-48dc-b5ef-0486dbfb0d3d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "f30267f9-ab49-41e8-80f7-8cda8e3dbec4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "9f050548-9ff0-46d9-a45a-421b13744d25", "AQAAAAEAACcQAAAAEBjvJD9/7VZT56e9gdMRN+z+GAMLVmDV86IrkbKE92cjeI78m47rLGqvt3F/0vMbmg==", "6c0ba0b9-8142-4ca6-9d15-f8231bf94fb3" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Appointment_UserId",
+                table: "Appointment",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Animals_AspNetUsers_OwnerId",
+                table: "Animals",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_Animals_AnimalId",
+                table: "Appointment",
+                column: "AnimalId",
+                principalTable: "Animals",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_AspNetUsers_UserId",
+                table: "Appointment",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ DataAccess/Migrations/20220524094635_WithoutOnDeletePortfolioAddress.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class WithoutOnDeletePortfolioAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "d5afbc0b-15a2-497f-bac0-42a71f01a157");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "ad24bf56-43a5-47e0-893e-e260db72273f");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "bf77de74-b81f-4260-b5f8-1cb91ab66573");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "36624077-3b93-48e8-88ef-68130e619974");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2699f26a-1b8e-40bc-9704-b5609036247f", "AQAAAAEAACcQAAAAEC3hQZNtMNZ9lh9Ic49DMF2TPLnefSWwqfPHsr+IuzU4wHyeLh6BeSBb29EhOKrOqA==", "d38e74f5-8d06-4d9f-a690-277b033197c4" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "227abc7a-2edf-487c-8569-52d7891e35a7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "96ea0a31-c077-4e62-8e12-16b9da08c3ca");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "792a5d38-25df-4275-af3c-10cf0cbbaeeb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "76912dd8-aaa9-43ef-ba1f-fef762b7bb9d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "43461189-c21c-4aab-8f05-38035ebca55e", "AQAAAAEAACcQAAAAEAKzZdnuoQyTVaDEL1ni/QxbDuPffkIqgEfQw06oQAYwuZQf2v3dCD1XJNCVy3C7oQ==", "7817bca0-9249-4658-84dd-de09c8add482" });
+        }
+    }
+}
