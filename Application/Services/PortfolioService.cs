02924ee84920cea3a4d5b5c370bf9cf95aa8af25
YuraRov <yura.rov555@gmail.com>
2022-05-25 12:19:01 +0300

    Polished service for Portfolio entity

--- Application/Services/PortfolioService.cs
+++ Application/Services/PortfolioService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Interfaces;
 using Core.Interfaces.Repositories;
 using Core.Interfaces.Services;
 
@@ -7,32 +8,74 @@ namespace Application.Services
     public class PortfolioService : IPortfolioService
     {
         private readonly IPortfolioRepository _portfolioRepository;
+        private readonly ILoggerManager _loggerManager;
 
-        public PortfolioService(IPortfolioRepository portfolioRepository) => _portfolioRepository = portfolioRepository;
-        
-        public async Task<Portfolio> GetPortfolioByIdAsync(int id)
+        public PortfolioService(IPortfolioRepository portfolioRepository, ILoggerManager loggerManager)
         {
-            return await _portfolioRepository.GetPortfolioByIdAsync(id);
+            _portfolioRepository = portfolioRepository;
+            _loggerManager = loggerManager;
+        }
+        
+        public async Task<Portfolio> GetPortfolioByUserIdAsync(int id)
+        {
+            _loggerManager.LogInfo("Inside GetPortfolioByUserIdAsync");
+            var portfolio = await _portfolioRepository.GetPortfolioByUserIdAsync(id);
+            if (portfolio != null)
+            {
+                _loggerManager.LogInfo("Return from GetPortfolioByUserIdAsync");
+                return portfolio;
+            }
+
+            _loggerManager.LogError($"User with ID - {id} doesn't have a portfolio!");
+            throw new NullReferenceException();
         }
 
         public async Task<IEnumerable<Portfolio>> GetAllPortfoliosAsync()
         {
+            _loggerManager.LogInfo("Inside GetAllPortfoliosAsync");
             return await _portfolioRepository.GetAllPortfoliosAsync();
         }
 
         public async Task CreatePortfolioAsync(Portfolio portfolio)
         {
-            await _portfolioRepository.CreatePortfolioAsync(portfolio);
+            _loggerManager.LogInfo("Inside CreatePortfolioAsync");
+            var portfolioInTable = await _portfolioRepository.GetPortfolioByUserIdAsync(portfolio.UserId);
+            if (portfolioInTable == null)
+            {
+                _loggerManager.LogInfo("Creating user's portfolio...");
+                await _portfolioRepository.CreatePortfolioAsync(portfolio);
+            }
+
+            _loggerManager.LogError($"User with ID - {portfolio.UserId} has already a portfolio!");
+            throw new ArgumentException();
         }
 
-        public async Task<Portfolio> UpdatePortfolioAsync(Portfolio portfolio)
+        public async Task UpdatePortfolioAsync(Portfolio portfolio)
         {
-            return await _portfolioRepository.UpdatePortfolioAsync(portfolio);
+            _loggerManager.LogInfo("Inside UpdatePortfolioAsync");
+            var portfolioInTable = await _portfolioRepository.GetPortfolioByUserIdAsync(portfolio.UserId);
+            if (portfolioInTable != null)
+            {
+                _loggerManager.LogInfo("Updating user's portfolio...");
+                await _portfolioRepository.UpdatePortfolioAsync(portfolio);
+            }
+
+            _loggerManager.LogError($"User with ID - {portfolio.UserId} doesn't have a portfolio!");
+            throw new NullReferenceException();
         }
 
-        public async Task DeletePortfolioAsync(int id)
+        public async Task DeletePortfolioByUserIdAsync(int id)
         {
-            await _portfolioRepository.DeletePortfolioAsync(id);
+            _loggerManager.LogInfo("Inside DeletePortfolioByUserIdAsync");
+            var portfolioInTable = await _portfolioRepository.GetPortfolioByUserIdAsync(id);
+            if (portfolioInTable != null)
+            {
+                _loggerManager.LogInfo("Deleting user's portfolio...");
+                await _portfolioRepository.DeletePortfolioByUserIdAsync(id);
+            }
+
+            _loggerManager.LogError($"User with ID - {id} doesn't have a portfolio!");
+            throw new NullReferenceException();
         }
     }
 }
