d2fb5c263898b15b75421433e9c9da4ed4c86df1
YuraRov <yura.rov555@gmail.com>
2022-05-26 16:35:40 +0300

    Removed BuildingNumber in Address

--- Core/Entities/Address.cs
+++ Core/Entities/Address.cs
@@ -2,12 +2,11 @@
 {
     public class Address
     {
-        public string? City { get; set; }
-        public string? Street { get; set; }
-        public string? House { get; set; }
-        public byte? BuildingNumber { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
         public ushort? ApartmentNumber { get; set; }
-        public int ZipCode { get; set; }
+        public string? ZipCode { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
     }
