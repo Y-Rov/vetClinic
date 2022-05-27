d12f3dc7879262b16779a4fa420a23ce4b0d5c67
DarkenHouk <32226733+DarkenHouk@users.noreply.github.com>
2022-05-27 11:50:29 +0300

    Renamed SalaryValidator as SalatyViewModelValidator Added Rule for min Id

---
+++ WebApi/Validators/SalaryViewModelValidator.cs
@@ -0,0 +1,20 @@
+﻿using Core.ViewModels.SalaryViewModel;
+using FluentValidation;
+
+namespace WebApi.Validators
+{
+    public class SalaryViewModelValidator: AbstractValidator<SalaryViewModel>
+    {
+        public SalaryViewModelValidator()
+        {
+            RuleFor(dto => dto.EmployeeId)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(1)
+                .WithMessage("Id must me min = 1");
+
+            RuleFor(dto => dto.Value)
+                .GreaterThan(0)
+                .WithMessage("Salary value must be more than 0");
+        }
+    }
+}
