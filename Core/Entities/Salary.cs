44a29d32ea863a8f1c6646644cc115f51a55574c
Anastasia Stetsenko <mubagu905@gmail.com>
2022-06-09 18:02:28 +0300

    Added Data to Salary User has IEnurable<Salary>

--- Core/Entities/Salary.cs
+++ Core/Entities/Salary.cs
@@ -5,5 +5,6 @@ namespace Core.Entities
         public int EmployeeId { get; set; }
         public User Employee { get; set; }
         public decimal Value { get; set; }
+        public DateTime date { get; set; }
     }
 }
