3310f75e7ce8204170ae46b6fb6ce0652f3390d4
Anastasia Stetsenko <mubagu905@gmail.com>
2022-06-09 20:33:27 +0300

    added Salary migration

---
+++ DataAccess/Migrations/20220609172928_ChangeSalaryTable.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class ChangeSalaryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Salaries",
+                table: "Salaries");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Salaries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Salaries",
+                type: "DATETIME",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Salaries",
+                table: "Salaries",
+                column: "Id");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "c5bd58fb-8ffc-4d0d-a0e6-6984341f953b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "5b648256-e392-4229-8342-1823c71f220d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "66c9f11a-a0a8-497c-b479-2cf0b532299d");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "04ca866f-b124-4224-bef7-1d59f32fcd92");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "5787d213-be1a-4674-9b26-7ff3ad1ca450", "AQAAAAEAACcQAAAAEIkywEvc0IX1R4uKhvfpNC7d7Up2cUkd1fY+8kWmSRHR5iTd+pm+Jaw84CDaz4eQNA==", "a9cb6b4a-42be-4f20-8a21-c922a2455258" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Salaries_EmployeeId",
+                table: "Salaries",
+                column: "EmployeeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Salaries",
+                table: "Salaries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Salaries_EmployeeId",
+                table: "Salaries");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Salaries");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Salaries");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Salaries",
+                table: "Salaries",
+                column: "EmployeeId");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ConcurrencyStamp",
+                value: "f55bc062-eda5-474e-ac20-c2020adb96eb");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ConcurrencyStamp",
+                value: "7570ffe4-07eb-48fc-a37c-211dbdb893a4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ConcurrencyStamp",
+                value: "73d43bf3-b8e5-4c69-9251-a55b28a4e4af");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "ConcurrencyStamp",
+                value: "07bed065-25d1-4533-b779-79ede1fd3536");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "PasswordHash", "SecurityStamp" },
+                values: new object[] { "2c03eb91-f53b-4169-bfae-02ab75ce452c", "AQAAAAEAACcQAAAAEIhwL1k0PvJ3hyJ0B3DfgBHAUi190ds3TMsAB8NS1OkjjV/d15oBHWi2Bvjrtb+dFA==", "fc31edef-ec50-45f2-96f8-c16a53770ebe" });
+        }
+    }
+}
