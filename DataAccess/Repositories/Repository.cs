8a93799f53e591f6c281b28a6d76d3e5791bd6d3
YuraRov <yura.rov555@gmail.com>
2022-06-12 11:57:52 +0300

    Increased readability in Repository class

--- DataAccess/Repositories/Repository.cs
+++ DataAccess/Repositories/Repository.cs
@@ -8,29 +8,33 @@ namespace DataAccess.Repositories
     public class Repository<T> : IRepository<T>
         where T : class
     {
-        readonly ClinicContext context;
+        private readonly ClinicContext _clinicContext;
 
-        public Repository(ClinicContext context)
+        public Repository(ClinicContext clinicContext)
         {
-            this.context = context;
+            _clinicContext = clinicContext;
         }
 
         public IQueryable<T> GetQuery(
-            Expression<Func<T, bool>> filter,
-            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy,
+            Expression<Func<T, bool>>? filter,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy,
             string includeProperties = "")
         {
-            IQueryable<T> set = filter == null ? context.Set<T>() :
-                 context.Set<T>().Where(filter);
+            IQueryable<T> set = filter == null ? _clinicContext.Set<T>() :
+                 _clinicContext.Set<T>().Where(filter);
 
             if (!string.IsNullOrEmpty(includeProperties))
+            {
                 set = includeProperties.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
                     .Aggregate(set, (current, includeProperty)
                         => current.Include(includeProperty));
+            }
 
             if (orderBy != null)
+            {
                 set = orderBy(set);
-
+            }
+            
             return set;
         }
 
@@ -41,68 +45,87 @@ namespace DataAccess.Repositories
             bool asNoTracking = false)
         {
             var query = GetQuery(filter, orderBy, includeProperties);
+            
             if (asNoTracking)
-                return await query.AsNoTracking().ToListAsync();
-            return await query.ToListAsync();
+            {
+                var noTrackingResult = await query.AsNoTracking()
+                    .ToListAsync();
+                
+                return noTrackingResult;
+            }
+
+            var trackingResult = await query.ToListAsync();
+            return trackingResult;
         }
 
         public async Task<T?> GetFirstOrDefaultAsync(
-            Expression<Func<T, bool>> filter,
+            Expression<Func<T, bool>>? filter = null,
             string includeProperties = "",
             bool asNoTracking = false)
         {
-            var result = GetQuery(
+            var query = GetQuery(
                 filter: filter,
                 includeProperties: includeProperties,
                 orderBy: null);
 
             if (asNoTracking)
-                return await result
-                    .AsNoTracking()
+            {
+                var noTrackingResult = await query.AsNoTracking()
                     .FirstOrDefaultAsync();
 
-            return await result.FirstOrDefaultAsync();
+                return noTrackingResult;
+            }
+               
+            var trackingResult = await query.FirstOrDefaultAsync();
+            return trackingResult;
         }
 
         public async Task<T?> GetById(int id, string includeProperties = "")
         {
-            if(string.IsNullOrEmpty(includeProperties))
-                return await context.Set<T>().FindAsync(id);
+            if (string.IsNullOrEmpty(includeProperties))
+            {
+                return await _clinicContext.Set<T>().FindAsync(id);
+            }
+            
+            var result = await _clinicContext.Set<T>().FindAsync(id);
 
-            var result = await context.Set<T>().FindAsync(id);
-
-            IQueryable<T> set = context.Set<T>();
+            IQueryable<T> set = _clinicContext.Set<T>();
 
             set = includeProperties.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
                     .Aggregate(set, (current, includeProperty)
                         => current.Include(includeProperty));
+
             return await set.FirstOrDefaultAsync(entity => entity == result);
         }
 
         public void Delete(T entity)
         {
-            if (context.Entry(entity).State == EntityState.Detached)
-                context.Attach(entity);
-
-            context.Entry(entity).State = EntityState.Deleted;
+            if (_clinicContext.Entry(entity).State == EntityState.Detached)
+            {
+                _clinicContext.Attach(entity);
+            }
+            
+            _clinicContext.Entry(entity).State = EntityState.Deleted;
         }
 
         public void Update(T entity)
         {
-            if (context.Entry(entity).State == EntityState.Detached)
-                context.Attach(entity);
+            if (_clinicContext.Entry(entity).State == EntityState.Detached)
+            {
+                _clinicContext.Attach(entity);
+            }
 
-            context.Entry(entity).State = EntityState.Modified;
+            _clinicContext.Entry(entity).State = EntityState.Modified;
         }
 
         public async Task InsertAsync(T entity)
         {
-            await context.Set<T>().AddAsync(entity);
+            await _clinicContext.Set<T>().AddAsync(entity);
         }
 
         public async Task SaveChangesAsync()
         {
-            await context.SaveChangesAsync();
+            await _clinicContext.SaveChangesAsync();
         }
     }
 }
