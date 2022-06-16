23944e3e57d9dbc931aaff767013a55919708680
DeniesLie <lienko.d.k@gmail.com>
2022-06-16 23:57:43 +0300

    Added cors to Program.cs

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -9,8 +9,13 @@ builder.Host.UseSerilog((context, services, configuration) => configuration
 
 var config = builder.Configuration;
 builder.Services.AddServices(config);
+builder.Services.AddCors(config =>
+{
+    config.AddDefaultPolicy(config => config.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+}
+);
 
 var app = builder.Build();
-
+app.UseCors();
 app.UseIdentityServer();
 app.Run();
