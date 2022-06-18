59b61f807f71f7948fa69ee6db99f8d45415d0da
= <--add>
2022-06-18 22:12:14 +0300

    added include user property

--- Application/Services/SpecializationService.cs
+++ Application/Services/SpecializationService.cs
@@ -25,7 +25,7 @@ namespace Application.Services
 
         public async Task<Specialization> GetSpecializationByIdAsync(int id)
         {
-            Specialization specialization = await _repository.GetById(id, "ProcedureSpecializations.Procedure,ProcedureSpecializations,UserSpecializations");
+            Specialization specialization = await _repository.GetById(id, "ProcedureSpecializations.Procedure,ProcedureSpecializations,UserSpecializations.User");
             if (specialization is null)
             {
                 _logger.LogWarn($"Specialization with id: {id} not found");
