23c4c03fe80c9dfde1feb69b81f544d62a8f41b3
YuraRov <yura.rov555@gmail.com>
2022-06-20 12:17:25 +0300

    Polished Address functionality

---
+++ WebApi/Validators/AddressValidators/AddressCreateViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Core.ViewModels.AddressViewModels;
+using FluentValidation;
+
+namespace WebApi.Validators.AddressValidators
+{
+    public class AddressCreateViewModelValidator : AddressBaseViewModelValidator<AddressCreateViewModel>
+    {
+        public AddressCreateViewModelValidator()
+        {
+            RuleFor(viewModel => viewModel.Id)
+                .GreaterThan(0)
+                .WithMessage("User ID must be greater than 0");
+        }
+    }
+}
+
