878c1a15a982a9adcdd6646c9489a527193fc538
froizerner <froizerner@gmail.com>
2022-06-24 17:59:21 +0300

    Added service and controller tests

---
+++ Tests/Application.Test/ExceptionServiceTests.cs
@@ -0,0 +1,138 @@
+﻿using Application.Test.Fixtures;
+using Core.Entities;
+using Core.Exceptions;
+using Core.Models;
+using Moq;
+
+namespace Application.Test
+{
+    public class ExceptionServiceTests : IClassFixture<ExceptionServiceFixture>
+    {
+        public ExceptionServiceTests(ExceptionServiceFixture exceptionServiceFixture)
+        {
+            _exceptionServiceFixture = exceptionServiceFixture;
+        }
+
+        private readonly ExceptionServiceFixture _exceptionServiceFixture;
+        private static readonly int _id = 1;
+
+        private static readonly ExceptionEntity _exceptionEntity = new()
+        {
+            Id = _id,
+            Name = "NotFoundException",
+            DateTime = DateTime.Now,
+            StackTrace = @" at Application.Services.ExceptionEntityService.GetAsync(Int32 id)",
+            Path = @"/api/exceptions/1"
+        };
+
+        private static readonly ExceptionStats _exceptionStat = new()
+        {
+            Count = 1,
+            Name = "NotFoundException"
+        };
+
+        private static readonly List<ExceptionStats> _exceptionStats = new() { _exceptionStat };
+        private static readonly List<ExceptionEntity> _exceptionEntities = new() { _exceptionEntity };
+
+        [Fact]
+        public async Task GetExceptionsAsync_Exceptions_ReturnsIEnumerableOfException()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetAsync())
+                .ReturnsAsync(_exceptionEntities);
+
+            // Act
+            var result = await _exceptionServiceFixture.MockExceptionEntityService.GetAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, _exceptionEntities);
+        }
+
+        [Fact]
+        public async Task GetExceptionAsync_Exception_ReturnException()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync(_exceptionEntity);
+
+            // Act
+            var result = await _exceptionServiceFixture.MockExceptionEntityService.GetAsync(_id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, _exceptionEntity);
+        }
+
+        [Fact]
+        public async Task GetExceptionAsync_Exception_ThrowsNotFoundException()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetAsync(It.IsAny<int>()))
+                .ReturnsAsync((ExceptionEntity)null!);
+
+            // Act
+            var result =  _exceptionServiceFixture.MockExceptionEntityService.GetAsync(_id);
+
+            // Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => result);
+        }
+
+        [Fact]
+        public async Task GetTodayExceptionsAsync_TodayExceptions_ReturnsIEnumerableOfException()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetTodayAsync())
+                .ReturnsAsync(_exceptionEntities);
+
+            // Act
+            var result = await _exceptionServiceFixture.MockExceptionEntityService.GetTodayAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, _exceptionEntities);
+
+            var resultAndExpect = result!.Zip(_exceptionEntities, (result, expect) => new { Result = result, Expect = expect });
+            foreach (var item in resultAndExpect)
+            {
+                Assert.Equal(item.Result.DateTime.Date, item.Expect.DateTime.Date);
+            }
+        }
+
+        [Fact]
+        public async Task GetExceptionStatsAsync_ExceptionStats_ReturnsIEnumerableOfExceptionStats()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetStatsAsync())
+                .ReturnsAsync(_exceptionStats);
+
+            // Act
+            var result = await _exceptionServiceFixture.MockExceptionEntityService.GetStatsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, _exceptionStats);
+        }
+
+        [Fact]
+        public async Task GetExceptionTodayStatsAsync_ExceptionTodayStats_ReturnsIEnumerableOfExceptionStats()
+        {
+            // Arrange
+            _exceptionServiceFixture.MockExceptionRepository
+                .Setup(r => r.GetTodayStatsAsync())
+                .ReturnsAsync(_exceptionStats);
+
+            // Act
+            var result = await _exceptionServiceFixture.MockExceptionEntityService.GetTodayStatsAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result, _exceptionStats);
+        }
+    }
+}
---
+++ Tests/Application.Test/Fixtures/ExceptionServiceFixture.cs
@@ -0,0 +1,29 @@
+﻿using Application.Services;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Interfaces;
+using Core.Interfaces.Repositories;
+using Moq;
+
+namespace Application.Test.Fixtures
+{
+    public class ExceptionServiceFixture
+    {
+        public ExceptionServiceFixture()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            MockExceptionEntityService = fixture.Freeze<ExceptionEntityService>();
+            MockExceptionRepository = fixture.Freeze<Mock<IExceptionEntityRepository>>();
+            MockLogger = fixture.Freeze<Mock<ILoggerManager>>();
+
+            MockExceptionEntityService = new ExceptionEntityService(
+                MockExceptionRepository.Object,
+                MockLogger.Object);
+        }
+
+        public ExceptionEntityService MockExceptionEntityService { get; }
+        public Mock<IExceptionEntityRepository> MockExceptionRepository { get; }
+        public Mock<ILoggerManager> MockLogger { get; }
+    }
+}
