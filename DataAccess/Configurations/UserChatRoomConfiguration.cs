53f26995906eb224294c3a101f42126e511f2686
Denys Liienko <lienko.d.k@gmail.com>
2022-06-28 18:36:31 +0300

    Added EF Core configurations

---
+++ DataAccess/Configurations/UserChatRoomConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations;
+
+public class UserChatRoomConfiguration : IEntityTypeConfiguration<UserChatRoom>
+{
+    public void Configure(EntityTypeBuilder<UserChatRoom> builder)
+    {
+        builder.HasKey(userChatRoom => new { userChatRoom.UserId, userChatRoom.ChatRoomId });
+
+        builder.HasOne(userChatRoom => userChatRoom.User)
+            .WithMany(u => u.UserChatRooms)
+            .HasForeignKey(userChatRoom => userChatRoom.UserId);
+
+        builder.HasOne(userChatRoom => userChatRoom.ChatRoom)
+            .WithMany(cr => cr.UserChatRooms)
+            .HasForeignKey(userChatRoom => userChatRoom.ChatRoomId);
+    }
+}
