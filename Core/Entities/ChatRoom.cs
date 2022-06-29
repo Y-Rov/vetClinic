ced2eaf01545e1c6df0a9f9e51d1226b0778fc34
Denys Liienko <lienko.d.k@gmail.com>
2022-06-29 16:52:07 +0300

    Removed IsActive property

--- Core/Entities/ChatRoom.cs
+++ Core/Entities/ChatRoom.cs
@@ -3,8 +3,6 @@
 public class ChatRoom
 {
     public int Id { get; set; }
-    public bool IsActive { get; set; }
-    
     public IEnumerable<UserChatRoom> UserChatRooms { get; set; } = new List<UserChatRoom>();
     public IEnumerable<Message> Messages { get; set; } = new List<Message>();
 }
