4f4274108ab20dd170ef51fde575d62988e15e69
Denys Liienko <lienko.d.k@gmail.com>
2022-06-29 11:24:09 +0300

    Added chatRoom service interface

---
+++ Application/Services/ChatRoomService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Entities;
+using Core.Interfaces.Services;
+
+namespace Application.Services;
+
+public class ChatRoomService : IChatRoomService
+{
+    public Task Create(ChatRoom chatRoom)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<User>> GetParticipants()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddMember(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task KickMember(int userId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task Deactivate()
+    {
+        throw new NotImplementedException();
+    }
+}
