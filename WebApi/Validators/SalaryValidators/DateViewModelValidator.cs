24b34807c00a220eab0b2d7e021576aa29f5ec01
Anastasia Stetsenko <mubagu905@gmail.com>
2022-06-30 18:18:01 +0300

    added validator

---
+++ WebApi/Validators/SalaryValidators/DateViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using Core.ViewModels.SalaryViewModel;
+using FluentValidation;
+
+
+namespace WebApi.Validators.SalaryValidators
+{
+    public class DateViewModelValidator: AbstractValidator<DateViewModel>
+    {
+        public DateViewModelValidator()
+        {
+            RuleFor(dto => dto.endDate)
+                .LessThanOrEqualTo(DateTime.Now)
+                .WithMessage("You try to get FinancialStatement for future");
+        }
+    }
+}
