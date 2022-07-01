8c9f46bb7215c32bc6016946bf0d005c46602d1e
= <--add>
2022-07-01 14:04:01 +0300

    added feedback entity

---
+++ DataAccess/Configurations/FeedbackConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccess.Configurations
+{
+    public class FeedbackConfiguration 
+        : IEntityTypeConfiguration<Feedback>
+    {
+        public void Configure(EntityTypeBuilder<Feedback> builder)
+        {
+            builder
+                .HasKey(feedback => feedback.Id);
+
+            builder
+                .HasOne(feedback => feedback.User)
+                .WithMany(user => user.Feedbacks)
+                .HasForeignKey(feedback => feedback.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.ToTable("Feedbacks");
+        }
+    }
+}
