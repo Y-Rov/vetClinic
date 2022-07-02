2d1e16b38efc8545b5bf60592b35b81d70617b21
Vitalik Knysh <knish.vital@gmail.com>
2022-07-02 22:05:37 +0300

    Changed default profile picture logic

--- Core/ViewModels/User/UserCreateViewModel.cs
+++ Core/ViewModels/User/UserCreateViewModel.cs
@@ -3,7 +3,7 @@
     public record UserCreateViewModel : UserBaseViewModel
     {
         public string? Email { get; init; }
-        public string ProfilePicture { get; init; } = string.Empty;
+        public string? ProfilePicture { get; init; }
         public string? Password { get; init; }
         public string? ConfirmPassword { get; init; }
     }
