1ece68dc83a276c989e2dfb83ec7fa5a8d7b6880
= <--add>
2022-07-02 22:33:38 +0300

    added feedback repository

---
+++ DataAccess/Repositories/FeedbackRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using DataAccess.Context;
+
+namespace DataAccess.Repositories
+{
+    public class FeedbackRepository : Repository<Feedback>, IFeedbackRepository
+    {
+        public FeedbackRepository(ClinicContext clinicContext) : base(clinicContext)
+        {
+        }
+    }
+}
