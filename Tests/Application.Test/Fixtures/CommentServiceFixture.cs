f9fd23fbea3152e0f7421e10c17ac94852716cc9
sefirus <sashasam348@gmail.com>
2022-07-03 10:45:38 +0300

    Added fixture

---
+++ Tests/Application.Test/Fixtures/CommentServiceFixture.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Interfaces;
+using Core.Interfaces.Repositories;
+using Moq;
+
+namespace Application.Test.Fixtures;
+
+public class CommentServiceFixture
+{
+    public CommentServiceFixture()
+    {
+        var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+        MockCommentRepository = fixture.Freeze<Mock<ICommentRepository>>();
+        MockLoggerManager = fixture.Freeze<Mock<ILoggerManager>>();
+    }
+    
+    public Mock<ICommentRepository> MockCommentRepository;
+    public Mock<ILoggerManager> MockLoggerManager;
+
+}
