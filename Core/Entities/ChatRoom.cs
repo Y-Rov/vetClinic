5b57e516ca2aea33dd32f6d6c0bc8cab464a372c
Denys Liienko <lienko.d.k@gmail.com>
2022-07-03 19:15:03 +0300

    Added type of ChatRoom

--- Core/Entities/ChatRoom.cs
+++ Core/Entities/ChatRoom.cs
@@ -1,8 +1,11 @@
-﻿namespace Core.Entities;
+﻿using Core.Emuns;
+
+namespace Core.Entities;
 
 public class ChatRoom
 {
     public int Id { get; set; }
+    public ChatType Type { get; set; }
     public IEnumerable<UserChatRoom> UserChatRooms { get; set; } = new List<UserChatRoom>();
     public IEnumerable<Message> Messages { get; set; } = new List<Message>();
 }
