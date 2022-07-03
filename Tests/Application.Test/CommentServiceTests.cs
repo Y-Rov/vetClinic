75390d86efc3be75bf5e27b942fb6cf0bec22b0c
sefirus <sashasam348@gmail.com>
2022-07-03 12:58:19 +0300

    temp

---
+++ Tests/Application.Test/CommentServiceTests.cs
@@ -0,0 +1,291 @@
+﻿using System.Linq.Expressions;
+using Application.Test.Fixtures;
+using Core.Entities;
+using Core.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace Application.Test;
+
+public class CommentServiceTests : IClassFixture<CommentServiceFixture>
+{
+    private readonly CommentServiceFixture _fixture;
+
+    public CommentServiceTests(CommentServiceFixture fixture)
+    {
+        _fixture = fixture;
+    }
+    
+    
+    
+    private readonly Comment _comment = new Comment()
+    {
+        Id = 1,
+        ArticleId = 1,
+        AuthorId = 1,
+        Content = "hello",
+        CreatedAt = new DateTime(2020, 10, 10, 10, 10, 10),
+        Edited = false
+    };
+    
+    private readonly IList<Comment> _comments = new List<Comment>()
+    {
+        new Comment()
+        {
+            Id = 1,
+            ArticleId = 1,
+            AuthorId = 1,
+            Content = "first hello",
+            CreatedAt = new DateTime(2020, 10, 10, 10, 10, 10),
+            Edited = false
+        },
+        new Comment()
+        {
+            Id = 2,
+            ArticleId = 1,
+            AuthorId = 1,
+            Content = "second hello",
+            CreatedAt = new DateTime(2020, 10, 10, 10, 10, 20),
+            Edited = false
+        },
+        new Comment()
+        {
+            Id = 1,
+            ArticleId = 2,
+            AuthorId = 1,
+            Content = "third hello",
+            CreatedAt = new DateTime(2020, 10, 10, 10, 10, 30),
+            Edited = true
+        }
+    };
+
+    private readonly User _requestUser = new User()
+    {
+        Id = 1,
+        FirstName = "Admin",
+        LastName = "Admin"
+    };
+    
+    [Fact]
+    public async Task GetAllCommentsAsync_whenCommentsListIsNotEmpty_thanReturnCommentsList()
+    {
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetAsync(
+                It.IsAny<Expression<Func<Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<Comment>, IOrderedQueryable<Comment>>>(),
+                It.IsAny<string>(),
+                It.IsAny<bool>()))
+            .ReturnsAsync(_comments);
+        
+        //Act
+        var result = await _fixture.MockCommentService.GetAllCommentsAsync();
+        
+        //Assert
+        Assert.NotEmpty(result);
+        Assert.Equal(_comments, result);
+    }
+    
+    [Fact]
+    public async Task GetAllCommentsAsync_whenCommentsListIsEmpty_thanReturnEmptyCommentsList()
+    {
+        //Arrange
+        var emptyComments = new List<Comment>();
+        
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetAsync(
+                It.IsAny<Expression<Func<Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<Comment>, IOrderedQueryable<Comment>>>(),
+                It.IsAny<string>(),
+                It.IsAny<bool>()))
+            .ReturnsAsync(emptyComments);
+        
+        //Act
+        var result = await _fixture.MockCommentService.GetAllCommentsAsync();
+        
+        //Assert
+        Assert.NotNull(result);
+        Assert.Empty(result);
+        Assert.Equal(emptyComments, result);
+    }
+    
+    [Fact]
+    public async Task GetByIdAsync_whenCommentExist_thanReturnTheComment()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetById(
+                It.Is<int>(x => x == _comment.Id), 
+                It.IsAny<string>()))
+            .ReturnsAsync(_comment);
+        
+        //Act
+        var result = await _fixture.MockCommentService.GetByIdAsync(_comment.Id);
+        
+        //Assert
+        Assert.NotNull(result);
+        Assert.Equal(_comment, result);
+    }
+    
+    [Fact]
+    public async Task GetByIdAsync_whenCommentDoesNotExist_thanThrowNotFound()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetById(
+                It.IsAny<int>(), 
+                It.IsAny<string>()))
+            .ReturnsAsync(() => null);
+        
+        //Act
+        var result = _fixture.MockCommentService.GetByIdAsync(10);
+        
+        //Assert
+        await Assert.ThrowsAsync<NotFoundException>(() => result);
+    }
+    
+    [Fact]
+    public async Task GetAllArticleCommentsAsync_whenCommentsListIsNotEmpty_thanReturnCommentsList()
+    {
+        int articleId = 1;
+
+        IList<Comment> articleComments = new List<Comment>()
+        {
+            new Comment()
+            {
+                Id = 1,
+                ArticleId = 1,
+                AuthorId = 1,
+                Content = "first hello",
+                CreatedAt = new DateTime(2020, 10, 10, 10, 10, 10),
+                Edited = false
+            },
+            new Comment()
+            {
+                Id = 2,
+                ArticleId = 1,
+                AuthorId = 1,
+                Content = "second hello",
+                CreatedAt = new DateTime(2020, 10, 10, 10, 10, 20),
+                Edited = false
+            }
+        };
+
+        Expression<Func<Comment, bool>> searchExpression = c => c.ArticleId == articleId;
+        
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetAsync(
+                It.IsAny<Expression<Func<Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<Comment>, IOrderedQueryable<Comment>>>(),
+                It.IsAny<string>(),
+                It.IsAny<bool>()))
+            .ReturnsAsync(articleComments);
+        
+        //Act
+        var result = await _fixture.MockCommentService.GetAllCommentsAsync();
+        
+        //Assert
+        Assert.NotEmpty(result);
+        Assert.Equal(articleComments, result);
+    }
+    
+    [Fact]
+    public async Task GetAllArticleCommentsAsync_whenCommentsListIsEmpty_thanReturnEmptyCommentsList()
+    {
+        //Arrange
+        var emptyComments = new List<Comment>();
+        
+        _fixture.MockCommentRepository
+            .Setup(repo => repo.GetAsync(
+                It.IsAny<Expression<Func<Comment, bool>>>(),
+                It.IsAny<Func<IQueryable<Comment>, IOrderedQueryable<Comment>>>(),
+                It.IsAny<string>(),
+                It.IsAny<bool>()))
+            .ReturnsAsync(emptyComments);
+        
+        //Act
+        var result = await _fixture.MockCommentService.GetAllCommentsAsync();
+        
+        //Assert
+        Assert.NotNull(result);
+        Assert.Empty(result);
+        Assert.Equal(emptyComments, result);
+    }
+
+    [Fact]
+    public async Task CreateCommentAsync_whenNormal_thenSuccess()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(r => r.InsertAsync(It.IsAny<Comment>()))
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+        
+        _fixture.MockCommentRepository
+            .Setup(r => r.SaveChangesAsync())
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+        //Act
+        await _fixture.MockCommentService.CreateCommentAsync(_comment);
+        //Assert
+        _fixture.MockCommentRepository
+            .Verify(r => r.InsertAsync(_comment), Times.Once);
+        _fixture.MockCommentRepository
+            .Verify(r => r.SaveChangesAsync(), Times.Once);
+    }
+    
+    [Fact]
+    public async Task CreateCommentAsync_whenUserDontExist_thenThrowNotFound()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(r => r.InsertAsync(It.IsAny<Comment>()))
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+
+        _fixture.MockCommentRepository
+            .Setup(r => r.SaveChangesAsync())
+            .Throws<DbUpdateException>();
+        //Act
+        var result = _fixture.MockCommentService.CreateCommentAsync(_comment);
+        //Assert
+        _fixture.MockCommentRepository
+            .Verify(r => r.InsertAsync(_comment), Times.Once);
+        await Assert.ThrowsAsync<NotFoundException>(() => result);
+    }
+    
+    [Fact]
+    public async Task DeleteCommentAsync_whenNormal_thenSuccess()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(r => r.De(It.IsAny<Comment>()))
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+        
+        _fixture.MockCommentRepository
+            .Setup(r => r.SaveChangesAsync())
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+        //Act
+        await _fixture.MockCommentService.CreateCommentAsync(_comment);
+        //Assert
+        _fixture.MockCommentRepository
+            .Verify(r => r.InsertAsync(_comment), Times.Once);
+        _fixture.MockCommentRepository
+            .Verify(r => r.SaveChangesAsync(), Times.Once);
+    }
+    
+    [Fact]
+    public async Task CreateCommentAsync_whenUserDontExist_thenThrowNotFound()
+    {
+        //Arrange
+        _fixture.MockCommentRepository
+            .Setup(r => r.InsertAsync(It.IsAny<Comment>()))
+            .Returns(Task.FromResult<object?>(null)).Verifiable();
+
+        _fixture.MockCommentRepository
+            .Setup(r => r.SaveChangesAsync())
+            .Throws<DbUpdateException>();
+        //Act
+        var result = _fixture.MockCommentService.CreateCommentAsync(_comment);
+        //Assert
+        _fixture.MockCommentRepository
+            .Verify(r => r.InsertAsync(_comment), Times.Once);
+        await Assert.ThrowsAsync<NotFoundException>(() => result);
+    }
+}
--- Tests/Application.Test/Fixtures/CommentServiceFixture.cs
+++ Tests/Application.Test/Fixtures/CommentServiceFixture.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture;
+﻿using Application.Services;
+using AutoFixture;
 using AutoFixture.AutoMoq;
 using Core.Interfaces;
 using Core.Interfaces.Repositories;
@@ -14,8 +15,13 @@ public class CommentServiceFixture
 
         MockCommentRepository = fixture.Freeze<Mock<ICommentRepository>>();
         MockLoggerManager = fixture.Freeze<Mock<ILoggerManager>>();
+        
+        MockCommentService = new CommentService(
+            MockCommentRepository.Object,
+            MockLoggerManager.Object);
     }
-    
+
+    public CommentService MockCommentService; 
     public Mock<ICommentRepository> MockCommentRepository;
     public Mock<ILoggerManager> MockLoggerManager;
 
