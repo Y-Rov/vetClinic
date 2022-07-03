8108bf1654e434fb6ee7f5b52689493573d16859
sefirus <sashasam348@gmail.com>
2022-07-03 23:50:52 +0300

    Created a fixture

---
+++ Tests/Application.Test/Fixtures/ArticleServiceFixture.cs
@@ -0,0 +1,37 @@
+﻿using Application.Services;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Interfaces;
+using Core.Interfaces.Repositories;
+using Core.Interfaces.Services;
+using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Application.Test.Fixtures;
+
+public class ArticleServiceFixture
+{
+    public ArticleServiceFixture()
+    {
+        var fixture =
+            new Fixture().Customize(new AutoMoqCustomization());
+
+        MockArticleRepository = fixture.Freeze<Mock<IArticleRepository>>();
+        MockLoggerManager = fixture.Freeze<Mock<ILoggerManager>>();
+        MockConfiguration = fixture.Freeze<Mock<IConfiguration>>();
+        MockImageService = fixture.Freeze<Mock<IImageService>>();
+
+        MockArticleService = new ArticleService(
+            MockArticleRepository.Object,
+            MockLoggerManager.Object,
+            MockConfiguration.Object,
+            MockImageService.Object);
+    }
+    
+    public ArticleService MockArticleService { get; }
+    public Mock<IArticleRepository> MockArticleRepository { get; }
+    public Mock<ILoggerManager> MockLoggerManager { get; }
+    public Mock<IConfiguration> MockConfiguration { get; }
+    public Mock<IImageService> MockImageService { get; }
+
+}
