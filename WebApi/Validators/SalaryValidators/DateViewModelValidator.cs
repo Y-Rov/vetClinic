3be3e06f000622b1d5397556319a46befb509fae
Anastasia Stetsenko <mubagu905@gmail.com>
2022-07-04 14:34:42 +0300

    added one more validator for date

--- WebApi/Validators/SalaryValidators/DateViewModelValidator.cs
+++ WebApi/Validators/SalaryValidators/DateViewModelValidator.cs
@@ -9,6 +9,7 @@ namespace WebApi.Validators.SalaryValidators
         public DateViewModelValidator()
         {
             RuleFor(dto => dto.endDate)
+                .NotEqual(start => start.startDate)
                 .LessThanOrEqualTo(DateTime.Now)
                 .WithMessage("You try to get FinancialStatement for future");
         }
