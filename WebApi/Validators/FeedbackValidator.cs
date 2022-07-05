9d6c8b195769605b340ce806eb92bcb87377a364
= <--add>
2022-07-06 02:07:55 +0300

    added feedback validator

---
+++ WebApi/Validators/FeedbackValidator.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.ViewModels.FeedbackViewModels;
+using FluentValidation;
+
+namespace WebApi.Validators
+{
+    public class FeedbackValidator : AbstractValidator<FeedbackCreateViewModel>
+    {
+        public FeedbackValidator()
+        {
+            RuleFor(feedback => feedback.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Email is incorrect");
+
+            RuleFor(feedback => feedback.ServiceRate)
+                .NotEmpty()
+                .ExclusiveBetween(0, 5)
+                .WithMessage("Service rate must be in range from 1 to 4");
+
+            RuleFor(feedback => feedback.PriceRate)
+                .NotEmpty()
+                .ExclusiveBetween(0, 5)
+                .WithMessage("Price rate must be in range from 1 to 4");
+
+            RuleFor(feedback => feedback.SupportRate)
+                .NotEmpty()
+                .ExclusiveBetween(0, 5)
+                .WithMessage("Support rate must be in range from 1 to 4");
+
+            RuleFor(feedback => feedback.Suggestions)
+                .MaximumLength(500)
+                .WithMessage("Lenght must be less than 1000");
+        }
+    }
+}
