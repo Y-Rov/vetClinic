b566d4ff590980073c07ffed947e052061db4d12
Vitalik Knysh <knish.vital@gmail.com>
2022-07-06 11:55:46 +0300

    Returned CollateParameters

---
+++ Core/Models/CollateParameters.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Models
+{
+    public record CollateParameters
+    {
+        public string? FilterParam { get; init; }
+        public string? OrderByParam { get; init; }
+        public int? TakeCount { get; init; }
+        public int SkipCount { get; init; } = 0;
+    }
+}
