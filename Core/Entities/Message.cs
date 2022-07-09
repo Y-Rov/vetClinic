d1d484d169ae236988d8de313fa0264c94543df7
Denys Liienko <lienko.d.k@gmail.com>
2022-07-09 20:34:48 +0300

    Removed IsRead and DateTimeOffset

--- Core/Entities/Message.cs
+++ Core/Entities/Message.cs
@@ -4,8 +4,7 @@ public class Message
 {
     public int Id { get; set; }
     public string? Text { get; set; }
-    public DateTimeOffset SentAt { get; set; }
-    public bool IsRead { get; set; }
+    public DateTime SentAt { get; set; }
     public int SenderId { get; set; }
     public User Sender { get; set; } = null!;
 
--- Core/ViewModels/MessageViewModels/MessageGetViewModel.cs
+++ Core/ViewModels/MessageViewModels/MessageGetViewModel.cs
@@ -3,9 +3,7 @@
 public class MessageGetViewModel : MessageViewModelBase
 {
     public int Id { get; set; }
-    public string? Text { get; set; }
-    public DateTimeOffset SentAt { get; set; }
-    public bool IsRead { get; set; }
+    public DateTime SentAt { get; set; }
 
     public int SenderId { get; set; }
     public string? SenderName { get; set; }
