e72d16902fd7cf2599bc5c51562af4713b11d099
= <--add>
2022-07-10 17:41:58 +0300

    added fixture

---
+++ Tests/WebApi.Test/Fixtures/FeedbackControllerFixture.cs
@@ -0,0 +1,26 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using Core.Entities;
+using Core.Interfaces.Services;
+using Core.ViewModels.FeedbackViewModels;
+using Moq;
+using WebApi.AutoMapper.Interface;
+using WebApi.Controllers;
+
+namespace WebApi.Test.Fixtures
+{
+    public class FeedbackControllerFixture
+    {
+        public FeedbackControllerFixture()
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+
+            
+        }
+
+        public FeedbackController MockController { get; }
+        public Mock<IFeedbackService> MockFeedbackService { get; }
+        public Mock<IViewModelMapper<FeedbackCreateViewModel, Feedback>> MockCreateFeedbackMapper { get; }
+        public Mock<IViewModelMapper<IEnumerable<Feedback>, IEnumerable<FeedbackReadViewModel>>> MockListFeedbackMapper { get; }
+    }
+}
