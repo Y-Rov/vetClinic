129a7667e5eda3d980aa422ad92f44341b6c5eeb
Denys Liienko <lienko.d.k@gmail.com>
2022-07-15 21:26:19 +0300

    Added ChatType

--- Core/ViewModels/ChatRoomViewModels/ChatRoomGetViewModel.cs
+++ Core/ViewModels/ChatRoomViewModels/ChatRoomGetViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace Core.ViewModel.ChatRoomViewModels;
+﻿using Core.Emuns;
+
+namespace Core.ViewModels.ChatRoomViewModels;
 
 public class ChatRoomGetViewModel
 {
     public int Id { get; set; }
+    public ChatType Type { get; set; }
     public string? Name { get; set; }
 }
