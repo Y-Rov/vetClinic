4c44dcda3fe8c4b1a79468f88646722596a134ae
Denys Liienko <lienko.d.k@gmail.com>
2022-07-17 16:25:29 +0300

    Removed readerId from GetMessagesInChatRoomAsync

--- Core/Interfaces/Services/IMessageService.cs
+++ Core/Interfaces/Services/IMessageService.cs
@@ -5,7 +5,7 @@ namespace Core.Interfaces.Services;
 public interface IMessageService
 {
     Task<Message?> GetByIdAsync(int id);
-    Task<IEnumerable<Message>> GetMessagesInChatRoomAsync(int readerId, int chatRoomId, int skip, int take);
+    Task<IEnumerable<Message>> GetMessagesInChatRoomAsync(int chatRoomId, int skip, int take);
     Task<IEnumerable<Message>> GetUnreadMessagesAsync(int userId);
     Task CreateAsync(Message message);
     Task ReadMessageAsync(int readerId, int messageId);
