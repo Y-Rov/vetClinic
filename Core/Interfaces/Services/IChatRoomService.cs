b59e8088a6dc41c4dcb65a4a4668bdf758fe5255
Denys Liienko <lienko.d.k@gmail.com>
2022-07-17 16:23:25 +0300

    Removed unused methods

--- Core/Interfaces/Services/IChatRoomService.cs
+++ Core/Interfaces/Services/IChatRoomService.cs
@@ -6,9 +6,6 @@ public interface IChatRoomService
 {
     Task CreateAsync(ChatRoom chatRoom);
     Task<IEnumerable<ChatRoom>> GetChatRoomsForUserAsync(int userId);
-    Task<IEnumerable<User>> GetParticipantsAsync(int chatRoomId);
-    Task AddMemberAsync(int roomId, int userId);
-    Task KickMemberAsync(int roomId, int userId);
     Task<ChatRoom> EnsurePrivateRoomCreatedAsync(int memberId1, int memberId2);
     Task<UserChatRoom?> GetUserChatRoomAsync(int userId, int chatRoomId);
     Task<bool> ExistsAsync(int id);
