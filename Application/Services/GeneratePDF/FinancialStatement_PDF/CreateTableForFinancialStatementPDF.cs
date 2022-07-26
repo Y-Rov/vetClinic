2f7ad9d696bc99047e0c6312525710243c7a6d94
Anastasia Stetsenko <mubagu905@gmail.com>
2022-07-26 10:35:30 +0300

    updated CreateTableForFinancialStatement

--- Application/Services/GeneratePDF/FinancialStatement_PDF/CreateTableForFinancialStatementPDF.cs
+++ Application/Services/GeneratePDF/FinancialStatement_PDF/CreateTableForFinancialStatementPDF.cs
@@ -28,22 +28,28 @@ namespace Application.Services.GeneratePDF.FinancialStatement_PDF
             table.Columns.Add(column);
 
             //Add rows
-            string incomesList;
-            string expencesList;
+            StringBuilder incomesList ;
+            StringBuilder expencesList;
             foreach(var statement in listOfFinancialStatement)
             {
-                table.Rows.Add(new object[] {statement.Month, statement.TotalIncomes, statement.TotalExpences});
-                incomesList = "";
+                table.Rows.Add(new object[] 
+                {
+                    statement.Month, 
+                    statement.TotalIncomes.ToString("F2"), 
+                    statement.TotalExpences.ToString("F2") 
+                });
+
+                incomesList = new StringBuilder();
                 foreach(var income in statement.IncomesList)
                 {
-                    incomesList += " AppointmentID: "+ income.AppointmenId.ToString() 
-                        + "\tCost: " + income.Cost.ToString() + "\n";
+                    incomesList.AppendLine( " AppointmentID: "+ income.AppointmenId.ToString() 
+                        + "\tCost: " + income.Cost.ToString("F2"));
                 }
-                expencesList = "";
+                expencesList = new StringBuilder();
                 foreach(var expence in statement.ExpencesList)
                 {
-                    expencesList+= " " + expence.EmployeeName.ToString() 
-                        + "\tPayments:" + (expence.SalaryValue + expence.Premium).ToString() + "\n";
+                    expencesList.AppendLine(" " + expence.EmployeeName.ToString() 
+                        + "\tPayments:" + (expence.SalaryValue + expence.Premium).ToString("F2"));
                 }
                 table.Rows.Add(new object[] {"",incomesList,expencesList});
             }
